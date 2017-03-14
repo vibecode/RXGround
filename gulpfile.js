bfca503cc02fcd0f65f02360c0c1c2ff7c53584b
vibecode <rumashkin@gmail.com>
2017-03-14 23:22:11 +0100

    add client tasks

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ const gulp = require('gulp'),
     source = require('vinyl-source-stream'),
     browserify = require('browserify'),
     watchify = require('watchify'),
-    babelify = require('babelify');
+    babelify = require('babelify'),
+    path = require('path'),
+    fs = require('fs');
 
 gulp.task('scripts:server', () => {
   return gulp.src('./src-server/**/*.js')
@@ -13,5 +15,53 @@ gulp.task('scripts:server', () => {
 });
 
 gulp.task(
-    'watch:scripts:server',
-    gulp.series('scripts:server', () => gulp.watch('./src-server/**/*.js')));
+    'watch:scripts:server', gulp.series(
+        'scripts:server',
+        () => gulp.watch('./src-server/**/*.js', gulp.series('scripts: server'))));
+
+gulp.task('watch:scripts:client', () => {
+  const files = fs.readdirSync('./src-client');
+
+  for (let i = 0; i < files.length; i++) {
+    const file = files[i];
+    if (path.extname(file) !== '.js') {
+      continue;
+    }
+    initBundlerWatch(path.join('src-client', file))
+  }
+
+  return gulp.watch('./src-client/**/*.js')
+             .on('change', initBundlerWatch)
+});
+
+gulp.task('watch:scripts', gulp.parallel('watch:scripts:client', 'watch:scripts:server'));
+
+let bundlers = {};
+function initBundlerWatch(file) {
+  if(bundlers.hasOwnProperty(file)) {
+    return;
+  }
+  const bundler = createBundler(file);
+  bundlers[file] = bundler;
+
+  const watcher = watchify(bundler);
+  const filename = path.basename(file);
+
+  function bundle() {
+    return bundler
+        .bundle()
+        .on('error', error => console.log(error))
+        .pipe(source(filename))
+        .pipe(gulp.dest('./public/build'));
+  }
+
+  watcher.on('update', bundle);
+  watcher.on('time', time => console.log(`Built client in ${time} ms`));
+  bundle();
+}
+
+function createBundler(file) {
+  const bundler = browserify(file);
+  bundler.transform(babelify);
+  return bundler;
+}
